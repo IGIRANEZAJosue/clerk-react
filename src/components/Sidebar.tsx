9104ca4e4e7ea5379ec0f00eec082737f4852cc8
Josue Igiraneza <igiranezaj28@gmail.com>
2024-12-01 20:33:55 +0200

    add react router dom

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,6 @@
 export default function Sidebar() {
   return (
-    <aside className="bg-gray-800 font-inconsolata text-white h-screen p-4 ">
+    <aside className="bg-gray-100 font-inconsolata text-gray-900 text-start h-screen p-4 ">
       <div className="flex items-start justify-start">
         <h1 className="text-5xl">Sidebar.tsx</h1>
       </div>
