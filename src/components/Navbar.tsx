0affd6525116528599f7232e05be228215b7d7e0
Josue Igiraneza <igiranezaj28@gmail.com>
2024-12-03 20:16:27 +0200

    feat: add title relevant title to dashboard navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,12 @@
 import { UserButton } from "@clerk/clerk-react";
+import { useLocation } from "react-router-dom";
 export default function Navbar() {
+	const location = useLocation();
+	const title = location.pathname.split("/").pop() || "dashboard";
+
 	return (
 		<div className="flex items-center font-lato justify-between p-4 border-y-2 border-gray-300 w-full">
-			<h1 className="font-bold text-3xl">Dashboard</h1>
+			<h1 className="font-bold text-3xl capitalize">{title}</h1>
 			<UserButton />
 		</div>
 	);
