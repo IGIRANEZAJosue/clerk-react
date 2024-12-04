4a5395bf71994183d9d50b1c19e7682859aa8950
Josue Igiraneza <igiranezaj28@gmail.com>
2024-12-04 10:16:02 +0200

    feat: add other routes

--- src/App.tsx
+++ src/App.tsx
@@ -7,27 +7,35 @@ import ContactPage from "./pages/contact";
 import Login from "./pages/login";
 import SignUpPage from "./pages/signup";
 import Settings from "./pages/dashboard/settings";
+import Calendar from "./pages/dashboard/calendar";
+import Projects from "./pages/dashboard/projects";
+import Team from "./pages/dashboard/team";
+import Documents from "./pages/dashboard/documents";
 
 const routes = createBrowserRouter([
-  {
-    element: <DefaultLayout />,
-    children: [
-      { path: "/", element: <Home /> },
-      { path: "/contact", element: <ContactPage /> },
-      { path: "/login", element: <Login /> },
-      { path: "/signup", element: <SignUpPage /> },
-      {
-        element: <DashboardLayout />,
-        path: "dashboard",
-        children: [
-          { index: true, element: <Dashboard /> },
-          { path: "settings", element: <Settings /> },
-        ],
-      },
-    ],
-  },
+	{
+		element: <DefaultLayout />,
+		children: [
+			{ path: "/", element: <Home /> },
+			{ path: "/contact", element: <ContactPage /> },
+			{ path: "/login", element: <Login /> },
+			{ path: "/signup", element: <SignUpPage /> },
+			{
+				element: <DashboardLayout />,
+				path: "dashboard",
+				children: [
+					{ index: true, element: <Dashboard /> },
+					{ path: "team", element: <Team /> },
+					{ path: "projects", element: <Projects /> },
+					{ path: "calendar", element: <Calendar /> },
+					{ path: "documents", element: <Documents /> },
+					{ path: "settings", element: <Settings /> },
+				],
+			},
+		],
+	},
 ]);
 
 export default function App() {
-  return <RouterProvider router={routes} />;
+	return <RouterProvider router={routes} />;
 }
